a61caea3a9b3ef16164bc042c59cf9d828bc8220
knlkshyp <knlkshyp@gmail.com>
2018-01-20 18:27:27 +0530

    Used params

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose.js');
-var {list} = require('./models/todo.js');
-var {user} = require('./models/user.js');
+var {
+    mongoose
+} = require('./db/mongoose.js');
+var {
+    list
+} = require('./models/todo.js');
+var {
+    user
+} = require('./models/user.js');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/lists',(req, res) => {
+app.post('/lists', (req, res) => {
     var newList = new list({
         text: req.body.text
     });
@@ -19,12 +26,34 @@ app.post('/lists',(req, res) => {
     });
 });
 
-app.get('/lists',(req, res) => {
+app.get('/lists', (req, res) => {
     list.find().then((todos) => {
-        res.send({todos});
+        res.send({
+            todos
+        });
     }, (err) => {
         res.send(err);
     });
+});
+
+app.get('/lists/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    list.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+
 });
 
 app.listen(3000, () => {
